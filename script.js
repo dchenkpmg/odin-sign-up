373aef915b108bde2393dfbd4d4a56bac64e342b
dylanchen1 <dylanchen1@kpmg.co.nz>
2024-03-01 16:04:59 +1300

    update form validation

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector('#registrationForm').addEventListener('submit', (event) => {
+  var password = document.querySelector('#password').value;
+  var confirmPassword = document.querySelector('#confirm');
+
+  if (password !== confirmPassword.value) {
+    document.querySelector('#error').style.color = "red";
+    document.querySelector('#error').style.fontSize = "12px";
+    document.querySelector('#error').textContent = "*The passwords do not match.";
+    event.preventDefault(); // prevent form submission. 
+  } else {
+    document.querySelector('#error').textContent = "";
+  }
+});
